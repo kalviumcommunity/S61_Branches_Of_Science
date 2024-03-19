a74c83935b189b8c60446a0b81cd4f0c16d4f770
Harshith200525 <Harshith.a@kalvium.community>
2024-03-19 14:10:27 +0530

    Created react app

---
+++ Backend/Model/model.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose')
+
+const branchSchema = new mongoose.Schema({
+    "ID": Number,
+    "Title": String,
+    "Toughness": Number,
+    "Needed basics": Array,
+    "Popularity": Number,
+    "Time takes": String,
+    "Overall grade": Number,
+    "Description": String,
+})
+
+const Branch = mongoose.model('branches', branchSchema)
+
+module.exports = Branch
