c72056339300ce047f8aee577aa960f7b69e6b38
Harshith200525 <Harshith.a@kalvium.community>
2024-03-21 15:36:29 +0530

    Fetching and displaying data

--- Frontend/src/components/Entity.jsx
+++ Frontend/src/components/Entity.jsx
@@ -1,16 +1,26 @@
 import { CardContent } from "@mui/material";
-import data from "../../data.json";
 import Card from "@mui/material/Card";
+import { useEffect, useState } from "react";
 
 const Entity = () => {
+  const [fetchedData, setFetchedData] = useState(null)
+
+  
+  useEffect(() => {
+    fetch("https://s61-branches-of-science-1.onrender.com/api/data").then((res) => res.json()).then((something) => setFetchedData(something.data));
+  },[])
+
   return (
     <div
       style={{
         display: "flex",
-        alignItems: "center",
+        justifyContent: "center",
+        flexWrap: "wrap",
       }}
     >
-      <Card
+      {
+        fetchedData && fetchedData.map((data) => (
+          <Card
         style={{
           borderRadius: "8px",
           boxShadow: "0 4px 8px rgba(0, 0, 0, 0.1)",
@@ -18,28 +28,28 @@ const Entity = () => {
           backgroundColor: "#ffffff",
           width: "35%",
           height: "75%",
-          position: "absolute",
-          top: "50%",
-          left: "50%",
-          transform: "translate(-50%,-50%)",
           display: "flex",
           justifyContent: "space-evenly",
+          margin: "25px",
           alignItems: "center",
           flexDirection: "column",
           textAlign: "center",
         }}
+        key={data["ID"]}
       >
         <CardContent>
-          <div style={{ fontSize: "40px", padding:'10px' }}>{data.id}</div>
-          <div style={{padding: '10px', fontSize: '20px', fontWeight: 'bold'}}>{data.title}</div>
-          <div style={{padding: '10px', fontSize: '20px'}}>Toughness: {data.toughness}</div>
-          <div style={{padding: '10px', fontSize: '20px'}}>Required basics: {data.neededBasics}</div>
-          <div style={{padding: '10px', fontSize: '20px'}}>Popularity: {data.popularity}</div>
-          <div style={{padding: '10px', fontSize: '20px'}}>Time needed: {data.timeTaken}</div>
-          <div style={{padding: '10px', fontSize: '20px'}}>Overall grade: {data.overallGrade}</div>
-          <div style={{padding: '10px', fontSize: '20px'}}>{data.description}</div>
+          <div style={{ fontSize: "40px", padding:'10px' }}>{data["ID"]}</div>
+          <div style={{padding: '10px', fontSize: '20px', fontWeight: 'bold'}}>{data["Title"]}</div>
+          <div style={{padding: '10px', fontSize: '20px'}}>Toughness: {data["Toughness"]}</div>
+          <div style={{padding: '10px', fontSize: '20px'}}>Required basics: {data["Needed Basics"]}</div>
+          <div style={{padding: '10px', fontSize: '20px'}}>Popularity: {data["Popularity"]}</div>
+          <div style={{padding: '10px', fontSize: '20px'}}>Time needed: {data["Time takes"]}</div>
+          <div style={{padding: '10px', fontSize: '20px'}}>Overall grade: {data["Overall grade"]}</div>
+          <div style={{padding: '10px', fontSize: '20px'}}>{data["Description"]}</div>
         </CardContent>
       </Card>
+        ))
+      }
     </div>
   );
 };
