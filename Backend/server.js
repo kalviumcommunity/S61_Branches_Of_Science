802d4714270fc30e1a418c47b1fda25c080268a4
Harshith200525 <Harshith.a@kalvium.community>
2024-03-21 14:44:59 +0530

    Added cors

--- Backend/server.js
+++ Backend/server.js
@@ -1,10 +1,12 @@
 const bodyParser = require('body-parser')
 const express = require('express')
 const mongoose = require('mongoose')
+const cors = require('cors')
 require('dotenv').config()
 const app = express()
 const port = 8000
 
+app.use(cors())
 app.use(express.json());
 app.use(bodyParser.json());
 
